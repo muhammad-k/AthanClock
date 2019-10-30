cb4278a9fad6e2716d3ecb40b994af9805c2188c
muhammad-k <muhammad@khalil.cx>
2019-10-29 23:53:44 -0400

    Having some issues with Maven dependencies. Have to figure this out.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,4 +1,8 @@
 package com.company;
+import java.util.Locale;
+import javax.speech.Central;
+import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerModeDesc;
 
 public class Main {
 
